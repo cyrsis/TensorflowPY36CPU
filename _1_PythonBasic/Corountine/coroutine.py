d65c2a56f8283e62064435f077c9123c030ba243
Victor <em3888@gmail.com>
2017-12-24 13:35:22 +0800

    Working on the dir and confirm Docker is running

--- _1_PythonBasic/Corountine/coroutine.py
+++ _1_PythonBasic/Corountine/coroutine.py
@@ -1,6 +1,12 @@
 import asyncio
 
+@asyncio.coroutine
 async def example():
     await asyncio.sleep(5)
     return 5
+
+if __name__ == '__main__':
+    scheduler = asyncio.get_event_loop()
+    f = asyncio.async(example())
+    f2 = asyncio.async(example(),loop= scheduler)
 
---
+++ _8_TensorFlowBasics/MNIST/DeepMnistPluralsight.py
@@ -0,0 +1,21 @@
+# victor
+# 23/12/2017
+# Created @ 2017-12-23 18:36
+
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+from tensorflow.python.framework import ops
+
+ops.reset_default_graph()
+
+
+sess = tf.Session()
+
+mnist = input_data("/Data/MNIST/", one_hot=True)
+
+train = pd.read(mnist)
+
+print(train.shape)
