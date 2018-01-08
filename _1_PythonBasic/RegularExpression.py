4b617ae88920ad0cc7f82ac48cee4febb0da749b
cyrsis <em3888@gmail.com>
2018-01-08 17:42:04 +0800

    Working with regular expression to clean data

---
+++ _1_PythonBasic/RegularExpression.py
@@ -0,0 +1,13 @@
+# Import the regular expression module
+import re
+
+# Compile the pattern: prog
+prog = re.compile('\d{3}-\d{3}-\d{4}')
+
+# See if the pattern matches
+result = prog.match('123-456-7890')
+print(bool(result))
+
+# See if the pattern matches
+result = prog.match('1123-456-7890')
+print(bool(result))
