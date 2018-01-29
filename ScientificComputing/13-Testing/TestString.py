b2e7183d8c7b0226d710548fc6a22b9eeacbe8c4
cyrsis <em3888@gmail.com>
2018-01-29 15:45:07 +0800

    Doing some clean up

--- ScientificComputing/13-Testing/TestString.py
+++ ScientificComputing/13-Testing/TestString.py
@@ -1,7 +1,8 @@
 import unittest
-import os # used for, for example, deleting files
+import os  # used for, for example, deleting files
 
 from find_in_file import find_string, NotFoundError
+
 
 class TestFindInFile(unittest.TestCase):
     def setUp(self):
@@ -9,15 +10,19 @@ class TestFindInFile(unittest.TestCase):
         file.write('aha')
         file.close()
         self.file = open('test_file.txt', 'r')
+
     def tearDown(self):
         self.file.close()
         os.remove(self.file.name)
+
     def test_exists(self):
-        line_no=find_string(self.file, 'aha')
+        line_no = find_string(self.file, 'aha')
         self.assertEqual(line_no, 0)
+
     def test_not_exists(self):
         self.assertRaises(NotFoundError, find_string,
-                                              self.file, 'bha')
+                          self.file, 'bha')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     unittest.main()
