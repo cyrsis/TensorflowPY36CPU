329a0bb289de3fd7a16bb565ea18a8b2afb0c0dc
cyrsis <em3888@gmail.com>
2018-02-09 15:52:00 +0800

    lot of clean up

--- _1_PythonBasic/special.py
+++
@@ -1,5 +0,0 @@
-from getpass import getpass
-from pprint import pprint
-
-password = getpass('Password: ')
-pprint([{1: 2, 3: 4}, {5: 6, 7: list(range(25))}])
--- _1_PythonBasic/while2.py
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-myList = [ 'English', 'Chinese', 'Japanese', 'German', 'France' ]
-while len(myList) > 0:
-    print("pop element out :", myList.pop())
