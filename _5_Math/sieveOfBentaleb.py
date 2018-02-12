637c5dcdd30372d0a4520e4d9bd4122e53b3c8b0
Victor <em3888@gmail.com>
2018-02-12 23:40:56 +0800

    Add lot of changes

--- _5_Math/sieveOfBentaleb.py
+++ _5_Math/sieveOfBentaleb.py
@@ -1,20 +1,20 @@
 import time
 
-print ('Enter a number to print all primes less or equal it')
+print('Enter a number to print all primes less or equal it')
 num = int(input())
 startTime = time.clock()
-isPrime = [True] * (num+1)
+isPrime = [True] * (num + 1)
 isPrime[0] = False
 isPrime[1] = False
-print ('These are the primes <= ' + str(num))
+print('These are the primes <= ' + str(num))
 primeList = []
-for i in range(2, num+1):
+for i in range(2, num + 1):
     if isPrime[i]:
         primeList.append(i)
-#AT THIS POINT EVERY NUMBER IS PRIME UNLESS
-# IT HAS BEEN REMOVED BECAUSE ITS A MULTIPLE OF ANOTHER PRIME
-        for j in range(i*2, num+1, i):
+        # AT THIS POINT EVERY NUMBER IS PRIME UNLESS
+        # IT HAS BEEN REMOVED BECAUSE ITS A MULTIPLE OF ANOTHER PRIME
+        for j in range(i * 2, num + 1, i):
             isPrime[j] = False
 endTime = time.clock()
 print(primeList)
-print('It took : ' + str(endTime-startTime))
+print('It took : ' + str(endTime - startTime))
